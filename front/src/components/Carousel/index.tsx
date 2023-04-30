9f76342e72fd86f0ef983d25a940a0990dcb8a72
Patrick <patrick-rossetto1@outlook.com>
2023-04-30 11:01:03 -0300

    carousel logica

--- front/src/components/Carousel/index.tsx
+++ front/src/components/Carousel/index.tsx
@@ -39,31 +39,39 @@ function Carousel({ fotos }: IProps) {
                     }
                 })}
             </div>
-            <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carousel"
-                data-bs-slide="prev"
-            >
-                <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carousel"
-                data-bs-slide="next"
-            >
-                <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                ></span>
+            <>
+                {fotos.length > 1 ? (
+                    <>
+                        <button
+                            className="carousel-control-prev"
+                            type="button"
+                            data-bs-target="#carousel"
+                            data-bs-slide="prev"
+                        >
+                            <span
+                                className="carousel-control-prev-icon"
+                                aria-hidden="true"
+                            ></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button
+                            className="carousel-control-next"
+                            type="button"
+                            data-bs-target="#carousel"
+                            data-bs-slide="next"
+                        >
+                            <span
+                                className="carousel-control-next-icon"
+                                aria-hidden="true"
+                            ></span>
 
-                <span className="visually-hidden">Next</span>
-            </button>
+                            <span className="visually-hidden">Next</span>
+                        </button>{' '}
+                    </>
+                ) : (
+                    <></>
+                )}
+            </>
         </Container>
     );
 }
